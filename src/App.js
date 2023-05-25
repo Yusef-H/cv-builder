92366d27f512b3bad3eeead1c767ad90b9715a3e
Joseph <yusefhalabi456@gmail.com>
2023-05-25 21:10:46 +0300

    change App and Preview to functional components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,45 @@
 import './styles/App.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Form from './components/Form';
 import Preview from './components/Preview';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'JohnDoe@gmail.com',
-      phone: '0550000999',
-      experiences: [{
-        jobTitle: 'Software Engineer',
-        company: 'ABC Corporation',
-        startDate: '2022-01-01',
-        endDate: '2023-05-01',
-        description: 'Worked on developing and maintaining web applications using modern technologies.'
-      }],
-      educations: [{
-        degreeType: 'Computer Science',
-        university: 'ABC University',
-        startDate: '2018-09-01',
-        endDate: '2022-06-30',
-        description: 'Studied computer science and gained knowledge in programming, algorithms, and software development.'
-      }]
-    };
+const App = () => {
+  const [state, setState] = useState({
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'JohnDoe@gmail.com',
+    phone: '0550000999',
+    experiences: [{
+      jobTitle: 'Software Engineer',
+      company: 'ABC Corporation',
+      startDate: '2022-01-01',
+      endDate: '2023-05-01',
+      description: 'Worked on developing and maintaining web applications using modern technologies.'
+    }],
+    educations: [{
+      degreeType: 'Computer Science',
+      university: 'ABC University',
+      startDate: '2018-09-01',
+      endDate: '2022-06-30',
+      description: 'Studied computer science and gained knowledge in programming, algorithms, and software development.'
+    }]
+  });
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.addToArrayProp = this.addToArrayProp.bind(this);
-    this.updateArrayProp = this.updateArrayProp.bind(this);
-    this.deleteArrayProp = this.deleteArrayProp.bind(this);
-  }
 
-  handleInputChange(event) {
+
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    setState((prevState) => ({
+      ...prevState,
+      [name] : value
+    }));
   }
+  
 
-  addToArrayProp(type) {
+  const addToArrayProp = (type) => {
     if(type === 'experience'){
-      this.setState((prevState) => ({
+      setState((prevState) => ({
+        ...prevState,
         experiences: [...prevState.experiences, {
           jobTitle: '',
           company: '',
@@ -51,7 +50,8 @@ class App extends Component {
       }));
     }
     else{
-      this.setState((prevState) => ({
+      setState((prevState) => ({
+        ...prevState,
         educations: [...prevState.educations, {
           degreeType: '',
           university: '',
@@ -63,51 +63,53 @@ class App extends Component {
     }
   }
 
-  updateArrayProp(newData, type, index) {
+  const updateArrayProp = (newData, type, index) => {
     if(type === 'experience'){
-      this.setState((prevState) => {
+      setState((prevState) => {
         const updatedExperiences = [...prevState.experiences];
         updatedExperiences[index] = newData;
-        return { experiences: updatedExperiences };
+        return { ...prevState, experiences: updatedExperiences };
       });
     }
     else{
-      this.setState((prevState) => {
+      setState((prevState) => {
         const updatedEducations = [...prevState.educations];
         updatedEducations[index] = newData;
-        return { educations: updatedEducations };
+        return { ...prevState, educations: updatedEducations };
       });
     }
   };
 
-  deleteArrayProp(type, index){
-    this.setState((prevState) => {
+  const deleteArrayProp = (type, index) => {
+    setState((prevState) => {
       const newArrayProp = [...(type ==='experience' ? prevState.experiences : prevState.educations)];
       newArrayProp.splice(index, 1);
-      return { [type+ 's']: newArrayProp };
+      return { 
+        ...prevState,
+        [type+ 's']: newArrayProp };
     })
   }
 
-  render() {
-    return (
-      <>
-        <h1>CV Builder With React</h1>
-        <div className="App">
-          <Form
-            {...this.state}
-            handleInputChange={this.handleInputChange}
-            addToArrayProp={this.addToArrayProp}
-            updateArrayProp={this.updateArrayProp}
-            deleteArrayProp={this.deleteArrayProp}
-          />
-        </div>
-        <h1>Preview:</h1>
-        <div className="Preview"> {/* Change to preview styles later */ }
-          <Preview {...this.state}/>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>CV Builder With React</h1>
+      <div className="App">
+        <Form
+          {...state}
+          handleInputChange={handleInputChange}
+          addToArrayProp={addToArrayProp}
+          updateArrayProp={updateArrayProp}
+          deleteArrayProp={deleteArrayProp}
+        />
+      </div>
+      <h1>Preview:</h1>
+      <div className="Preview"> {/* Change to preview styles later */ }
+        <Preview {...state}/>
+      </div>
+    </>
+  );
 }
+
+
 
 export default App;
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,69 +1,65 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../styles/Preview.css';
-class Preview extends Component{
-
-    render() {
-        const { firstName, lastName, email, phone, experiences, educations } = this.props;
-        
-        return (
-            <div>
-                <div className="header">
-                    <div className="left">
-                        <h1>{firstName} {lastName}</h1>
-                        <p>{email}</p>
-                    </div>
-                    <div className="right">
-                        <p>{phone}</p>
-                    </div>
+const Preview = ({ firstName, lastName, email, phone, experiences, educations}) => {
+    return (
+        <div>
+            <div className="header">
+                <div className="left">
+                    <h1>{firstName} {lastName}</h1>
+                    <p>{email}</p>
                 </div>
-                <hr />
-                <div className="experiences-section">
-                    {experiences.length > 0 && <h2 className="exp-title">Experiences</h2>}
-                    {experiences.map((experience, index) => (
-                        <div key={index} className="exp-container">
-                            <div className="job-title-info">
-                                <div className="titles">
-                                    <p>{experience.jobTitle} </p>
-                                    <p>At</p>
-                                    <p>{experience.company}</p>
-                                </div>
-                                <div className="dates">
-                                    <p>From: </p>
-                                    <p>{experience.startDate}</p>
-                                    <p>Until: </p>
-                                    <p>{experience.endDate}</p>
-                                </div>
-                            </div>
-                            <p className="desc">{experience.description}</p>
-                        </div>
-                    ))}
+                <div className="right">
+                    <p>{phone}</p>
                 </div>
-
-                <div className="educations-section">
-                    {educations.length > 0 && <h2 className="exp-title">Education</h2>}
-                    {educations.map((education, index) => (
-                        <div key={index} className="edu-container">
-                            <div className="edu-header">
-                                <div className="titles">
-                                    <p>{education.degreeType} </p>
-                                    <p>At</p>
-                                    <p>{education.university}</p>
-                                </div>
-                                <div className="dates">
-                                    <p>From: </p>
-                                    <p>{education.startDate}</p>
-                                    <p>Until: </p>
-                                    <p>{education.endDate}</p>
-                                </div>
-                            </div>
-                            <p className="desc">{education.description}</p>
-                        </div>
-                    ))}
-                </div>
-
             </div>
-        )
-    }
+            <hr />
+            <div className="experiences-section">
+                {experiences.length > 0 && <h2 className="exp-title">Experiences</h2>}
+                {experiences.map((experience, index) => (
+                    <div key={index} className="exp-container">
+                        <div className="job-title-info">
+                            <div className="titles">
+                                <p>{experience.jobTitle} </p>
+                                <p>At</p>
+                                <p>{experience.company}</p>
+                            </div>
+                            <div className="dates">
+                                <p>From: </p>
+                                <p>{experience.startDate}</p>
+                                <p>Until: </p>
+                                <p>{experience.endDate}</p>
+                            </div>
+                        </div>
+                        <p className="desc">{experience.description}</p>
+                    </div>
+                ))}
+            </div>
+
+            <div className="educations-section">
+                {educations.length > 0 && <h2 className="exp-title">Education</h2>}
+                {educations.map((education, index) => (
+                    <div key={index} className="edu-container">
+                        <div className="edu-header">
+                            <div className="titles">
+                                <p>{education.degreeType} </p>
+                                <p>At</p>
+                                <p>{education.university}</p>
+                            </div>
+                            <div className="dates">
+                                <p>From: </p>
+                                <p>{education.startDate}</p>
+                                <p>Until: </p>
+                                <p>{education.endDate}</p>
+                            </div>
+                        </div>
+                        <p className="desc">{education.description}</p>
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    )
 }
+
 
 export default Preview;
